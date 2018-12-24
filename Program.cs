dea8d5035da52d622d981a93cbdb5725ff543ab4
Tomasz Kisielewski <tomek.kisielewski@gmail.com>
2018-12-24 21:51:57 +0100

    Add basic window

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using SDL2;
 
 namespace LogicGates
 {
@@ -6,7 +7,31 @@ namespace LogicGates
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            SDL.SDL_Init(SDL.SDL_INIT_VIDEO);
+            var window = SDL.SDL_CreateWindow("LogicGates",
+                                        SDL.SDL_WINDOWPOS_CENTERED,
+                                        SDL.SDL_WINDOWPOS_CENTERED,
+                                        1024,
+                                        768,
+                                        SDL.SDL_WindowFlags.SDL_WINDOW_RESIZABLE);
+
+            SDL.SDL_Event e;
+            bool quit = false;
+
+            while (!quit)
+            {
+                while (SDL.SDL_PollEvent(out e) != 0)
+                {
+                    switch (e.type)
+                    {
+                        case SDL.SDL_EventType.SDL_QUIT:
+                            quit = true;
+                            break;
+                    }
+                }
+            }
+            SDL.SDL_DestroyWindow(window);
+            SDL.SDL_Quit();
         }
     }
 }
