d1766d79486efcfa4329d65d905ac7ec2f4836ec
Tomasz Kisielewski <tomek.kisielewski@gmail.com>
2018-12-24 23:37:43 +0100

    Easier debug with SDL

--- Models/Engine/Output.cs
+++ Models/Engine/Output.cs
@@ -8,6 +8,9 @@ namespace LogicGates.Models
         IntPtr _window = IntPtr.Zero;
         public Output()
         {
+            #if DEBUG
+                SDL.SDL_SetHint(SDL.SDL_HINT_WINDOWS_DISABLE_THREAD_NAMING, "1");
+            #endif
             SDL.SDL_Init(SDL.SDL_INIT_VIDEO);
             _window = SDL.SDL_CreateWindow("LogicGates", SDL.SDL_WINDOWPOS_CENTERED, SDL.SDL_WINDOWPOS_CENTERED,
                                             1024, 768, SDL.SDL_WindowFlags.SDL_WINDOW_RESIZABLE);
