32f4a9faf01b58b432d848d5fd127f365173a4d3
Tomasz Kisielewski <tomek.kisielewski@gmail.com>
2018-12-24 23:49:46 +0100

    Add error handling

--- Models/Engine/Output.cs
+++ Models/Engine/Output.cs
@@ -11,9 +11,21 @@ namespace LogicGates.Models
             #if DEBUG
                 SDL.SDL_SetHint(SDL.SDL_HINT_WINDOWS_DISABLE_THREAD_NAMING, "1");
             #endif
-            SDL.SDL_Init(SDL.SDL_INIT_VIDEO | SDL.SDL_INIT_AUDIO);
+
+            if (SDL.SDL_Init(SDL.SDL_INIT_VIDEO | SDL.SDL_INIT_AUDIO) < 0)
+            {
+                Console.WriteLine($"Unable to initialize SDL. Error: {SDL.SDL_GetError()}");
+                return;
+            }
+
             _window = SDL.SDL_CreateWindow("LogicGates", SDL.SDL_WINDOWPOS_CENTERED, SDL.SDL_WINDOWPOS_CENTERED,
                                             1024, 768, SDL.SDL_WindowFlags.SDL_WINDOW_RESIZABLE);
+
+            if (_window == IntPtr.Zero)
+            {
+                Console.WriteLine($"Unable to create a window. SDL. Error: {SDL.SDL_GetError()}");
+                return;
+            }
         }
 
         ~Output()
