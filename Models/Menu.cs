3c34078ea69aae1c51c35901e7ef481d5323f6e9
Tomasz Kisielewski <tomek.kisielewski@gmail.com>
2018-12-25 22:38:44 +0100

    Basic Menu class

---
+++ Models/Menu.cs
@@ -0,0 +1,26 @@
+using System;
+using SDL2;
+using LogicGates.Engine;
+
+namespace LogicGates.Models
+{
+    public class Menu : Asset
+    {
+        public Menu(string fileName) : base(fileName)
+        {
+        }
+
+        public Menu(string fileName, Size size) : base(fileName, size)
+        {
+        }
+
+        public Menu(string fileName, Size size, Position position) : base(fileName, size, position)
+        {
+        }
+
+        public override void Render(IntPtr renderer)
+        {
+            Drawer.RenderTexture(Texture, renderer, Position.Width, Position.Heigth, Size.Width, Size.Heigth);
+        }
+    }
+}
