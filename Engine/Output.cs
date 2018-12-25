76fcfe1bf425c9b5ec6c4cd918a11417f18bf8eb
Tomasz Kisielewski <tomek.kisielewski@gmail.com>
2018-12-25 02:27:11 +0100

    Add renderer, change private field to prop

--- Engine/Output.cs
+++ Engine/Output.cs
@@ -5,7 +5,7 @@ namespace LogicGates.Engine
 {
     public class Output
     {
-        IntPtr _window = IntPtr.Zero;
+        public IntPtr Window { get; private set; } = IntPtr.Zero;
         public Output()
         {
             #if DEBUG
@@ -13,25 +13,47 @@ namespace LogicGates.Engine
             #endif
 
             if (SDL.SDL_Init(SDL.SDL_INIT_VIDEO | SDL.SDL_INIT_AUDIO) < 0)
-            {
-                Console.WriteLine($"Unable to initialize SDL. Error: {SDL.SDL_GetError()}");
-                return;
-            }
+                SDLLogger.Fatal(nameof(SDL.SDL_Init));
 
-            _window = SDL.SDL_CreateWindow("LogicGates", SDL.SDL_WINDOWPOS_CENTERED, SDL.SDL_WINDOWPOS_CENTERED,
+            Window = SDL.SDL_CreateWindow("LogicGates", SDL.SDL_WINDOWPOS_CENTERED, SDL.SDL_WINDOWPOS_CENTERED,
                                             1024, 768, SDL.SDL_WindowFlags.SDL_WINDOW_RESIZABLE);
 
-            if (_window == IntPtr.Zero)
-            {
-                Console.WriteLine($"Unable to create a window. SDL. Error: {SDL.SDL_GetError()}");
-                return;
-            }
+            if (Window == IntPtr.Zero)
+                SDLLogger.Fatal(nameof(SDL.SDL_CreateWindow),
+                () => ReleaseAndQuit(IntPtr.Zero, IntPtr.Zero, IntPtr.Zero, IntPtr.Zero));
+
+            var renderer = SDL.SDL_CreateRenderer(Window, -1,
+                SDL.SDL_RendererFlags.SDL_RENDERER_ACCELERATED | SDL.SDL_RendererFlags.SDL_RENDERER_PRESENTVSYNC);
+
+            if(renderer == IntPtr.Zero)
+                SDLLogger.Fatal(nameof(SDL.SDL_CreateRenderer),
+                    () => ReleaseAndQuit(Window, IntPtr.Zero, IntPtr.Zero, IntPtr.Zero));
         }
 
         ~Output()
         {
-            SDL.SDL_DestroyWindow(_window);
+            SDL.SDL_DestroyWindow(Window);
             SDL.SDL_Quit();
+        }
+
+        private bool ReleaseAndQuit(IntPtr window, IntPtr renderer, IntPtr font, IntPtr image)
+        {
+            if(window != IntPtr.Zero)
+                SDL.SDL_DestroyWindow(window);
+
+            if(renderer != IntPtr.Zero)
+                SDL.SDL_DestroyRenderer(renderer);
+
+            if(font != IntPtr.Zero)
+                SDL_ttf.TTF_CloseFont(font);
+
+            if(image != IntPtr.Zero)
+                SDL.SDL_DestroyTexture(image);
+
+            SDL_image.IMG_Quit();
+            SDL.SDL_Quit();
+
+            return false;
         }
     }
 }
