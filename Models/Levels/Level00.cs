24a3927cefe16fdb7cdcd40ecf5f8d2f9cc7ad80
Tomasz Kisielewski <tomek.kisielewski@gmail.com>
2018-12-25 18:55:20 +0100

    Zero level - menu (just background)

--- Models/Levels/Level00.cs
+++ Models/Levels/Level00.cs
@@ -1,10 +1,13 @@
+using System;
+using System.Collections.Generic;
+
 namespace LogicGates.Models
 {
     public class Level00 : Level
     {
         public Level00()
         {
-
+            AsstesList.Add(new Background("background.png", new Size {Width = 16, Heigth = 16}));
         }
     }
 }
