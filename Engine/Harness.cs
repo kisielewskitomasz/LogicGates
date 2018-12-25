909cdd2cc339f57b9b89cf0988dda45ace43a26c
Tomasz Kisielewski <tomek.kisielewski@gmail.com>
2018-12-25 18:50:41 +0100

    Create ctor for harness class

--- Engine/Harness.cs
+++ Engine/Harness.cs
@@ -4,6 +4,22 @@ namespace LogicGates.Engine
 {
     public class Harness
     {
+        public Engine.Output EngineOutput { get; set; } = null;
+        public Engine.Input EngineInput { get; set; } = null;
+        public Models.Level GameCurrentLevel { get; set; } = null;
 
+        public Harness()
+        {
+            EngineOutput = new Engine.Output();
+
+            GameCurrentLevel = new Models.Level00();
+
+            foreach (var item in GameCurrentLevel.AsstesList)
+            {
+                item.Render(Output.Renderer);
+            }
+
+            EngineInput = new Engine.Input();
+        }
     }
 }
