b068a779b428bd2f591d78ae6d2db6bb55b107b4
Tomasz Kisielewski <tomek.kisielewski@gmail.com>
2018-12-25 02:27:52 +0100

    Basic template of level class

--- Models/Level.cs
+++ Models/Level.cs
@@ -1,9 +1,14 @@
 using System;
-
+using System.Collections.Generic;
 namespace LogicGates.Models
 {
-    public class Level
+    public abstract class Level
     {
+        public List<Object> Objects { get; private set; } = null;
 
+        public Level()
+        {
+            Objects = new List<Object>();
+        }
     }
 }
