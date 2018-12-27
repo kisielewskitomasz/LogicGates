bbd957c7e9ea7ac5d6ea444523f5f321e4a4b0ab
Tomasz Kisielewski <tomek.kisielewski@gmail.com>
2018-12-28 00:35:38 +0100

    Using static instead of injecting

--- Engine/Input.cs
+++ Engine/Input.cs
@@ -2,14 +2,15 @@ using System;
 using System.Collections.Generic;
 using SDL2;
 using LogicGates.Models;
+using LogicGates.Engine;
 using LogicGates.Common;
 
 namespace LogicGates.Engine
 {
     public class Input
     {
-        bool _quit = false;
-        public Input(List<Asset> assetsList)
+        static bool _quit = false;
+        public Input()
         {
             SDL.SDL_Event e;
 
@@ -20,20 +21,22 @@ namespace LogicGates.Engine
                     switch (e.type)
                     {
                         case SDL.SDL_EventType.SDL_QUIT:
-                            Quit();
+                            Harness.QuitGame();
                             break;
                         case SDL.SDL_EventType.SDL_MOUSEBUTTONDOWN:
                             var mousePosition = new Position();
                                 if (SDL.SDL_GetMouseState(out mousePosition.Width, out mousePosition.Height) == SDL.SDL_BUTTON(SDL.SDL_BUTTON_LEFT))
                                 {
                                     System.Console.WriteLine($"Left click at: {mousePosition.Width}, {mousePosition.Height}");
-                                    foreach (var asset in assetsList)
+                                    var reversedAssetList = new List<Asset>(Harness.GameCurrentLevel.AsstesList);
+                                    reversedAssetList.Reverse();
+                                    foreach (var asset in reversedAssetList)
                                     {
                                         if ((mousePosition.Width >= asset.Position.Width) && (mousePosition.Width <= (asset.Position.Width + asset.Size.Width)) &&
                                             (mousePosition.Height >= asset.Position.Height) && (mousePosition.Height <= (asset.Position.Height + asset.Size.Height)))
                                         {
                                             System.Console.WriteLine($"Clicked on :{asset.ToString()}");
-                                            asset.Clicked(mousePosition, this);
+                                            asset.Clicked(mousePosition);
                                             break;
                                         }
                                     }
@@ -44,7 +47,7 @@ namespace LogicGates.Engine
             }
         }
 
-        public void Quit()
+        public static void Quit()
         {
             _quit = true;
         }
