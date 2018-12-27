f3e6dbbb13ac9b4d2d44818bbf0885bd524f215c
Tomasz Kisielewski <tomek.kisielewski@gmail.com>
2018-12-28 00:38:11 +0100

    Add final splash when level are out

---
+++ Models/Splash.cs
@@ -0,0 +1,41 @@
+using System;
+using System.IO;
+using SDL2;
+using LogicGates.Engine;
+using LogicGates.Common;
+
+namespace LogicGates.Models
+{
+    public class Splash : Asset
+    {
+        public Splash(string fileName) : base(fileName)
+        {
+        }
+
+        public Splash(string fileName, Size size) : base(fileName, size)
+        {
+        }
+
+        public Splash(string fileName, Size size, Position position) : base(fileName, size, position)
+        {
+        }
+
+        public override void Clicked(Position mousePosition)
+        {
+            if ((mousePosition.Width >= (Position.Width + 32)) && (mousePosition.Width <= (Position.Width + Size.Width - 32)))
+            {
+                if((mousePosition.Height >= Position.Height + 32 + (64 * 4)) && (mousePosition.Height <= (Position.Height + 32 + (64 * 5))))
+                    Back();
+            }
+        }
+
+        public override void Render() =>
+            Drawer.RenderTexture(Texture, Output.Renderer, Position.Width, Position.Height, Size.Width, Size.Height);
+
+        void Back()
+        {
+            Harness.GameCurrentLevel = new Level00();
+            Harness.RefreshOutput();
+        }
+    }
+}
