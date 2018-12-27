7d04ab0115b3dff35b27db4742f8cbfb689b7f37
Tomasz Kisielewski <tomek.kisielewski@gmail.com>
2018-12-28 00:50:50 +0100

    Add base64 encoding decoding for game save

--- Engine/Harness.cs
+++ Engine/Harness.cs
@@ -7,39 +7,33 @@ using LogicGates.Models;
 
 namespace LogicGates.Engine
 {
-    public class Harness
-    {
+    public class Harness {
         public static Engine.Output EngineOutput { get; set; } = null;
         public static Engine.Input EngineInput { get; set; } = null;
         public static Models.Level GameCurrentLevel { get; set; } = null;
 
-        public Harness()
-        {
+        public Harness() {
             EngineOutput = new Engine.Output();
             GameCurrentLevel = new Models.Level00();
             RefreshOutput();
             EngineInput = new Engine.Input();
         }
 
-        public static void RefreshOutput()
-        {
+        public static void RefreshOutput() {
             GameCurrentLevel.AsstesList.ForEach((asset) => asset.Render());
             SDL.SDL_RenderPresent(Output.Renderer);
         }
 
-        public static void LoadGame()
-        {
+        public static void LoadGame() {
             System.Console.WriteLine("Load game");
-            try
-            {
+            try {
                 System.Console.WriteLine("load file");
-                using (StreamReader inputFile = new StreamReader(FilePath.Get("save.dat")))
-                {
-                    string lastLevel = inputFile.ReadToEnd().TrimEnd();
+                using (StreamReader inputFile = new StreamReader(FilePath.Get("save.dat"))) {
+                    string lastLevel = Base64Decode(inputFile.ReadToEnd().TrimEnd());
+                    System.Console.WriteLine($"Load level: {lastLevel}");
                     if (lastLevel.Equals("LogicGates.Models.Level00"))
                         Harness.GameCurrentLevel = new Level01();
-                    else
-                    {
+                    else {
                         Type type = Type.GetType(lastLevel, false);
                         if (type == null)
                             Harness.GameCurrentLevel = new Level99();
@@ -50,30 +44,36 @@ namespace LogicGates.Engine
                     Harness.RefreshOutput();
                 }
             }
-            catch (FileNotFoundException)
-            {
+            catch (FileNotFoundException) {
                 SaveGame();
             }
         }
 
-        public static void SaveGame()
-        {
-            using (StreamWriter outputFile = new StreamWriter(FilePath.GetNotExisting("save.dat")))
-            {
-                outputFile.WriteLine(Harness.GameCurrentLevel);
+        public static void SaveGame() {
+            using (StreamWriter outputFile = new StreamWriter(FilePath.GetNotExisting("save.dat"))){
+                System.Console.WriteLine($"Save level: {Harness.GameCurrentLevel.ToString()}");
+                outputFile.WriteLine(Base64Encode(Harness.GameCurrentLevel.ToString()));
             }
         }
 
-        public static void QuitGame()
-        {
+        public static void QuitGame() {
             Harness.SaveGame();
             Input.Quit();
         }
 
-        public static void ResetGame()
-        {
+        public static void ResetGame() {
             Harness.GameCurrentLevel = new Level01();
             Harness.RefreshOutput();
+        }
+
+        static string Base64Encode(string plainText) {
+            var plainTextBytes = System.Text.Encoding.UTF8.GetBytes(plainText);
+            return System.Convert.ToBase64String(plainTextBytes);
+        }
+
+        static string Base64Decode(string base64EncodedData) {
+            var base64EncodedBytes = System.Convert.FromBase64String(base64EncodedData);
+            return System.Text.Encoding.UTF8.GetString(base64EncodedBytes);
         }
     }
 }
