2cbcfd2d3b9983b605fc80d61d4694578940a3c8
Tomasz Kisielewski <tomek.kisielewski@gmail.com>
2018-12-27 22:30:06 +0100

    Mechanism to detect click on top (lastest) asset

--- Engine/Harness.cs
+++ Engine/Harness.cs
@@ -1,5 +1,7 @@
 using System;
+using System.Collections.Generic;
 using SDL2;
+using LogicGates.Models;
 
 namespace LogicGates.Engine
 {
@@ -12,7 +14,6 @@ namespace LogicGates.Engine
         public Harness()
         {
             EngineOutput = new Engine.Output();
-
             GameCurrentLevel = new Models.Level00();
 
             foreach (var item in GameCurrentLevel.AsstesList)
@@ -21,8 +22,9 @@ namespace LogicGates.Engine
             }
 
             SDL.SDL_RenderPresent(Output.Renderer);
-
-            EngineInput = new Engine.Input();
+            var reverseAssetList = new List<Asset>(GameCurrentLevel.AsstesList);
+            reverseAssetList.Reverse();
+            EngineInput = new Engine.Input(reverseAssetList);
         }
     }
 }
