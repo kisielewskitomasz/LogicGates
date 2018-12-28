65536413db3b0dbd2db87cfc702e935a680b15bf
Tomasz Kisielewski <tomek.kisielewski@gmail.com>
2018-12-28 21:10:50 +0100

    Add examples gates with centering them

--- Models/Levels/Level01.cs
+++ Models/Levels/Level01.cs
@@ -4,13 +4,17 @@ using SDL2;
 using LogicGates.Common;
 using LogicGates.Engine;
 
-namespace LogicGates.Models
-{
-    public class Level01 : Level
-    {
-        public Level01()
-        {
+namespace LogicGates.Models {
+    public class Level01 : Level {
+        public Level01() {
             AsstesList.Add(new Tray("tray.png", new Size {Width = 1024, Height = 64}));
+            var trayGateList = new List<Gate>();
+            trayGateList.Add(new GateAND("symbol_and.png", new Size {Width = 48, Height = 48}, new Position {Height = 8}));
+            trayGateList.Add(new GateAND("symbol_nand.png", new Size {Width = 48, Height = 48}, new Position {Height = 8}));
+            foreach(var gate in trayGateList) {
+                gate.Position.Width = (Canvas.Width / 2) - (trayGateList.Count * gate.Size.Width / 2) + (trayGateList.IndexOf(gate) * gate.Size.Width);
+                AsstesList.Add(gate);
+            }
             Harness.SaveGame();
         }
     }
