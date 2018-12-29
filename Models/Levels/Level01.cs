bbd16412054b973e633f5f32681ea84515109c5f
Tomasz Kisielewski <tomek.kisielewski@gmail.com>
2018-12-29 01:42:47 +0100

    Put new elements on level

--- Models/Levels/Level01.cs
+++ Models/Levels/Level01.cs
@@ -8,13 +8,15 @@ namespace LogicGates.Models {
     public class Level01 : Level {
         public Level01() {
             AsstesList.Add(new Screens.Tray());
-            var trayGateList = new List<Gate>();
-            trayGateList.Add(new GateAND(new Position {Height = 8}));
-            trayGateList.Add(new GateNAND( new Position {Height = 8}));
-            foreach(var gate in trayGateList) {
-                gate.Position.Width = (Canvas.Width / 2) - (trayGateList.Count * gate.Size.Width / 2) + (trayGateList.IndexOf(gate) * gate.Size.Width);
+
+            var trayGatesList = new List<Gate>();
+            trayGatesList.Add(new GateAND(new Position {Height = 0}));
+            trayGatesList.Add(new GateNAND(new Position {Height = 0}));
+            foreach(var gate in trayGatesList) {
+                gate.Position.Width = (Canvas.Width / 2) - (trayGatesList.Count * gate.Size.Width / 2) + (trayGatesList.IndexOf(gate) * gate.Size.Width);
                 AsstesList.Add(gate);
             }
+
             var logicSourcesList = new List<Elements.Logic>();
             logicSourcesList.Add(new Elements.LogicOne(new Position {Width = 72}));
             logicSourcesList.Add(new Elements.LogicZero(new Position {Width = 72}));
@@ -22,6 +24,17 @@ namespace LogicGates.Models {
                 source.Position.Height = (Canvas.Height / 2) - (logicSourcesList.Count * source.Size.Height / 2) + (logicSourcesList.IndexOf(source) * source.Size.Height);
                 AsstesList.Add(source);
             }
+
+            var lampsList = new List<Elements.Lamp>();
+            lampsList.Add(new Elements.LampOn(new Position {Width = Canvas.Width - (72 * 3)}));
+            lampsList.Add(new Elements.LampOff(new Position {Width = Canvas.Width - (72 * 3)}));
+            foreach(var lamp in lampsList) {
+                lamp.Position.Height = (Canvas.Height / 2) - (lampsList.Count * lamp.Size.Height / 2) + (lampsList.IndexOf(lamp) * lamp.Size.Height);
+                AsstesList.Add(lamp);
+            }
+
+            AsstesList.Add(new Elements.Ground(new Position {Width = Canvas.Width - (72 * 2), Height = Canvas.Height - (72 * 3)}));
+
             Harness.SaveGame();
         }
     }
