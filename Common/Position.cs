0239e865e068c6bb000a0fd8ad5718cca4291c1c
Tomasz Kisielewski <tomek.kisielewski@gmail.com>
2018-12-30 13:11:50 +0100

    Overload plus and minus operator for position

--- Common/Position.cs
+++ Common/Position.cs
@@ -3,5 +3,19 @@ namespace LogicGates.Common
     public struct Position {
         public int Width;
         public int Height;
+
+        public static Position operator+ (Position a, Position b) {
+            Position position = new Position();
+            position.Width = a.Width + b.Width;
+            position.Height = a.Height + b.Height;
+            return position;
+        }
+
+        public static Position operator- (Position a, Position b) {
+            Position position = new Position();
+            position.Width = a.Width - b.Width;
+            position.Height = a.Height - b.Height;
+            return position;
+        }
     }
 }
