5ca5b9410ecb89786d7eee4284e69675882f219b
Tomasz Kisielewski <tomek.kisielewski@gmail.com>
2018-12-30 16:16:52 +0100

    Add missing ctors

--- Common/Position.cs
+++ Common/Position.cs
@@ -2,6 +2,12 @@ namespace LogicGates.Common
 {
     public class Position : Point
     {
+        public Position() : base()
+        {
+        }
 
+        public Position(int width, int height) : base(width, height)
+        {
+        }
     }
 }
--- Common/Size.cs
+++ Common/Size.cs
@@ -2,6 +2,12 @@ namespace LogicGates.Common
 {
     public class Size : Point
     {
+        public Size() : base()
+        {
+        }
 
+        public Size(int width, int height) : base(width, height)
+        {
+        }
     }
 }
