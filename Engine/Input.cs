5039ff0ed91bbd9a8df874396417caf9dc0d88f8
Tomasz Kisielewski <tomek.kisielewski@gmail.com>
2019-01-03 02:56:55 +0100

    Click & Move implementation, some refactoring

--- Engine/Input.cs
+++ Engine/Input.cs
@@ -10,59 +10,83 @@ namespace LogicGates.Engine
     public class Input
     {
         static bool _quit = false;
-        static bool _isMovable = false;
         static int _state = 0;
+
+        static Asset ClickedAsset { get; set; } = null;
+        static Position MousePosition { get; set; } = new Position();
+        static Position RelativeMousePosition { get; set; } = null;
+
         public Input()
         {
             SDL.SDL_Event e;
-            Asset clickedAsset = null;
 
             while (!_quit)
             {
                 while (SDL.SDL_PollEvent(out e) != 0)
                 {
-                    switch (e.type)
-                    {
-                        case SDL.SDL_EventType.SDL_QUIT:
-                            {
-                                Harness.QuitGame();
-                                break;
-                            }
-                        case SDL.SDL_EventType.SDL_MOUSEBUTTONDOWN:
-                            {
-                                var mousePosition = new Position();
-                                if (SDL.SDL_GetMouseState(out mousePosition.Width, out mousePosition.Height) == SDL.SDL_BUTTON(SDL.SDL_BUTTON_LEFT))
-                                {
-                                    System.Console.Write("Left ");
-                                    clickedAsset = FindClickedAsset(mousePosition);
-                                    clickedAsset.ClickedLeft(mousePosition);
-                                    if ((clickedAsset.IsMovable) && (_state == 0))
-                                    {
-                                        _state = 1;
-                                    }
-                                }
-
-                                else if (SDL.SDL_GetMouseState(out mousePosition.Width, out mousePosition.Height) == SDL.SDL_BUTTON(SDL.SDL_BUTTON_RIGHT))
-                                {
-                                    System.Console.Write("Right ");
-                                    clickedAsset = FindClickedAsset(mousePosition);
-                                    clickedAsset.ClickedRight(mousePosition);
-                                    _state = 0;
-                                }
-                                break;
-                            }
-                        case SDL.SDL_EventType.SDL_MOUSEMOTION:
-                            {
-                                if (_state == 1)
-                                {
-                                    var mousePosition = new Position();
-                                    SDL.SDL_GetMouseState(out mousePosition.Width, out mousePosition.Height);
-                                }
-                                break;
-                            }
-                    }
+                    HandleEvent(e);
                 }
             }
+        }
+        static void HandleEvent(SDL.SDL_Event e)
+        {
+            switch (e.type)
+            {
+                case SDL.SDL_EventType.SDL_QUIT:
+                    OnSDL_QUIT();
+                    break;
+                case SDL.SDL_EventType.SDL_MOUSEBUTTONDOWN:
+                    OnSDL_MOUSEBUTTONDOWN();
+                    break;
+                case SDL.SDL_EventType.SDL_MOUSEMOTION:
+                    OnSDL_MOUSEMOTION();
+                    break;
+            }
+        }
+
+        private static void OnSDL_MOUSEMOTION()
+        {
+            if (_state == 1)
+            {
+                SDL.SDL_GetMouseState(out MousePosition.Width, out MousePosition.Height);
+
+                ClickedAsset.Position = MousePosition + RelativeMousePosition;
+
+                Harness.RefreshOutput();
+            }
+        }
+
+        static void OnSDL_MOUSEBUTTONDOWN()
+        {
+            var clickedButton = SDL.SDL_GetMouseState(out MousePosition.Width, out MousePosition.Height);
+            if (clickedButton == SDL.SDL_BUTTON(SDL.SDL_BUTTON_LEFT))
+            {
+                System.Console.Write("Left ");
+                ClickedAsset = FindClickedAsset(MousePosition);
+
+                ClickedAsset.ClickedLeft(MousePosition);
+                if ((ClickedAsset.IsMovable) && (_state == 0))
+                {
+                    RelativeMousePosition = (ClickedAsset.Position - MousePosition);
+                    _state = 1;
+                }
+                else if ((ClickedAsset.IsMovable) && (_state == 1))
+                {
+                    _state = 0;
+                }
+            }
+            else if (clickedButton == SDL.SDL_BUTTON(SDL.SDL_BUTTON_RIGHT))
+            {
+                System.Console.Write("Right ");
+                ClickedAsset = FindClickedAsset(MousePosition);
+                ClickedAsset.ClickedRight(MousePosition);
+                _state = 0;
+            }
+        }
+
+        private static void OnSDL_QUIT()
+        {
+            Harness.QuitGame();
         }
 
         private static Asset FindClickedAsset(Position mousePosition)
